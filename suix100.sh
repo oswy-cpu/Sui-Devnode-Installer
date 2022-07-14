ff856409f64f3385d43619cca26023438eea8b65
oswy-cpu <74738120+oswy-cpu@users.noreply.github.com>
2022-07-14 16:44:42 +0300

    Update suix100.sh

--- suix100.sh
+++ suix100.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
+echo -e "\e[1m\e[32m1. Обновлюсь \e[0m" && sleep 1
 sudo apt update &> /dev/null
 apt-get update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends tzdata git ca-certificates curl build-essential libssl-dev pkg-config libclang-dev cmake &> /dev/null
 
-echo "\e[1m\e[32mУстановка RUST" && sleep 1
+echo "=================================================="
 
+echo -e "\e[1m\e[32m2. Устанавливаю раст \e[0m" && sleep 1
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
-echo "\e[1m\e[32mУстановка ноды" && sleep 1
+echo "=================================================="
 
+echo -e "\e[1m\e[32m3. Клонирую репозиторий \e[0m" && sleep 1
 cd $HOME
 rm -rf /var/sui/db /var/sui/genesis.blob
 systemctl stop sui-node.service
@@ -17,8 +21,9 @@ mkdir -p /var/sui/db
 git clone https://github.com/MystenLabs/sui.git
 cd sui
 
-echo "\e[1m\e[32mЕще немного настроить осталось" && sleep 1
+echo "=================================================="
 
+echo -e "\e[1m\e[32m4. ...компиляция... \e[0m" && sleep 1
 git remote add upstream https://github.com/MystenLabs/sui
 git fetch upstream
 git checkout -B devnet --track upstream/devnet
@@ -27,20 +32,12 @@ wget -P /var/sui https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genes
 cargo build -p sui-node --release
 mv $HOME/sui/target/release/sui-node /usr/local/bin/
 
-echo "\e[1m\e[32mИ сервисники создать" && sleep 1
+echo "=================================================="
+
+echo -e "\e[1m\e[32m5. Сервисы надо создать \e[0m"
 
 sed -i.bak "s/db-path:.*/db-path: \"\/var\/sui\/db\"/ ; s/genesis-file-location:.*/genesis-file-location: \"\/var\/sui\/genesis.blob\"/" /var/sui/fullnode.yaml
-while true; do
-    read -p "Открыть публичный доступ к API и метрикам ноды? [y/n] (только маленьки буквы)" rmv
-    rmv=${rmv,,}                                 
-    case $rmv in
-        [y]* )
-        sed -i.bak "s/127.0.0.1/0.0.0.0/" /var/sui/fullnode.yaml
-	    break;;
-        [n]* ) break;;
-        * ) echo "y/n и никак иначе ";;   
-    esac
-done
+sed -i.bak "s/127.0.0.1/0.0.0.0/" /var/sui/fullnode.yaml
 
 echo "[Unit]
 Description=Sui Node
@@ -56,20 +53,25 @@ RestartSec=120
 WantedBy=multi-user.target
 " > /etc/systemd/system/sui-node.service
 
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/ports_opening.sh) \ 9184 9000 8080
+echo "=================================================="
 
+# Starting services
 sudo systemctl restart systemd-journald
 sudo systemctl daemon-reload
 sudo systemctl enable sui-node.service
 sudo systemctl restart sui-node.service
 
-echo "\e[1m\e[32mУстановлено, запущено, работает." && sleep 1
+echo "=================================================="
+
+echo -e "\e[1m\e[32mNode Started \e[0m"
+
+echo "=================================================="
 
 echo -e "\e[1m\e[32mОстановить ноду: \e[0m" 
 echo -e "\e[1m\e[39m    systemctl stop sui-node \n \e[0m" 
 
-echo -e "\e[1m\e[32mЗапустить: \e[0m" 
+echo -e "\e[1m\e[32mЗапустить ноду: \e[0m" 
 echo -e "\e[1m\e[39m    systemctl start sui-node \n \e[0m" 
 
-echo -e "\e[1m\e[32mПроверить логи: \e[0m" 
+echo -e "\e[1m\e[32mTПроверка логов: \e[0m" 
 echo -e "\e[1m\e[39m    journalctl -u sui-node -f \n \e[0m" 
